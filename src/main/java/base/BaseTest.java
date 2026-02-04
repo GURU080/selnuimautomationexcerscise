daddd2a8c6ce0193a32f9bcf63e934a7775aad79
gurutirangaaerospace@gmail.com <guruprasadm1502@gamil.com>
2026-02-04 19:14:02 +0530

    loggoing added

--- src/main/java/base/BaseTest.java
+++ src/main/java/base/BaseTest.java
@@ -4,28 +4,37 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class BaseTest {
-	
-	protected WebDriver driver;
-	
-	@BeforeMethod
-	public void setup() {
-		
-	   driver =new ChromeDriver();
-		driver.get("https://automationexercise.com/login");
-		driver.manage().window().maximize();
-	}
-	
-	
-	@AfterMethod
-	public void teardown() {
-		
-		if (driver  != null ) {
-			driver.close();
-		}
-	}
-	
 
+    protected WebDriver driver;
+
+    // Logger instance for this class
+    protected static final Logger logger =
+            LogManager.getLogger(BaseTest.class);
+
+    @BeforeMethod
+    public void setup() {
+
+        logger.info("Launching Chrome browser");
+
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+        logger.info("Navigating to login page");
+        driver.get("https://automationexercise.com/login");
+    }
+
+    @AfterMethod
+    public void teardown() {
+
+        logger.info("Closing browser");
+
+        if (driver != null) {
+            driver.quit();   // ✅ better than close()
+        }
+    }
 }
--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -1,26 +1,32 @@
 package tests;
 
-import static org.testng.Assert.assertEquals;
-
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import base.BaseTest;
 import pages.LoginPage;
 
-public class LoginTest  extends BaseTest{
-	
-	@Test
-	public void testvalidlogin(){
-		
-		LoginPage loginpage =new LoginPage(driver);
-		loginpage.enterEmail("guruprasadm1502@gmail.com");
-		loginpage.enterPassword("12345");
-		loginpage.clickLogin();
-		System.out.println("titel of the page is :"+ driver.getTitle() );
-		
-		Assert.assertEquals(driver.getTitle(), "Automation Exercise");
-	
-		
-	}
+public class LoginTest extends BaseTest {
+
+    @Test
+    public void testValidLogin() {
+
+        logger.info("Starting valid login test");
+
+        LoginPage loginPage = new LoginPage(driver);
+
+        logger.info("Entering email");
+        loginPage.enterEmail("guruprasadm1502@gmail.com");
+
+        logger.info("Entering password");
+        loginPage.enterPassword("12345");
+
+        logger.info("Clicking login button");
+        loginPage.clickLogin();
+
+        logger.info("Verifying page title after login");
+        Assert.assertEquals(driver.getTitle(), "Automation Exercise");
+
+        logger.info("Valid login test completed successfully");
+    }
 }
