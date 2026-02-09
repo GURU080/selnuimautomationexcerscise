6bd4473f02863778d8762d71877d2dcb50088fde
gurutirangaaerospace@gmail.com <guruprasadm1502@gamil.com>
2026-02-09 18:05:24 +0530

    first commit

---
+++ src/main/java/utils/ExtentManager.java
@@ -0,0 +1,30 @@
+package utils;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
+public class ExtentManager {
+
+    private static ExtentReports extent;
+
+    public static ExtentReports getExtent() {
+
+        if (extent == null) {
+
+            ExtentSparkReporter reporter =
+                    new ExtentSparkReporter("test-output/ExtentReport.html");
+
+            reporter.config().setReportName("Automation Test Report");
+            reporter.config().setDocumentTitle("QA Automation Results");
+
+            extent = new ExtentReports();
+            extent.attachReporter(reporter);
+
+            extent.setSystemInfo("Tester", "Guruprasad");
+            extent.setSystemInfo("Environment", "QA");
+            extent.setSystemInfo("Browser", "Chrome");
+        }
+
+        return extent;
+    }
+}
---
+++ src/test/java/listeners/ExtentTestListener.java
@@ -0,0 +1,44 @@
+package listeners;
+
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+
+import utils.ExtentManager;
+
+public class ExtentTestListener implements ITestListener {
+
+    private static ExtentReports extent = ExtentManager.getExtent();
+    private static ThreadLocal<ExtentTest> test = new ThreadLocal<>();
+
+    @Override
+    public void onTestStart(ITestResult result) {
+        ExtentTest extentTest =
+                extent.createTest(result.getMethod().getMethodName());
+        test.set(extentTest);
+    }
+
+    @Override
+    public void onTestSuccess(ITestResult result) {
+        test.get().pass("Test Passed");
+    }
+
+    @Override
+    public void onTestFailure(ITestResult result) {
+        test.get().fail(result.getThrowable());
+    }
+
+    @Override
+    public void onFinish(ITestContext context) {
+        extent.flush();
+    }
+    
+    @Override
+    public void onStart(ITestContext context) {
+        System.out.println("🔥 EXTENT LISTENER STARTED 🔥");
+    }
+
+}
